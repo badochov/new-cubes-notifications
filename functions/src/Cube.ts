3c12515800f92fa5de01f103b42500cf38f0fde9
Hubert Badocha <badocha.hubert@gmail.com>
2020-05-18 17:34:00 +0200

    change indexing to mails, made chron execute once in 30 minutes

--- functions/src/Cube.ts
+++ functions/src/Cube.ts
@@ -1,25 +1,27 @@
-export class Cube {
-	constructor(
-		readonly name: string,
-		readonly price: string,
-		readonly link: string
-	) {}
+import { firestore } from "firebase-admin";
 
-	toString() {
-		return name;
-	}
+export class Cube {
+  constructor(
+    readonly name: string,
+    readonly price: string,
+    readonly link: string
+  ) {}
+
+  toString(): string {
+    return this.link;
+  }
 }
 
 export const cubeConverter = {
-	toFirestore: (cube: Cube) => {
-		return {
-			name: cube.name,
-			price: cube.price,
-			link: cube.link,
-		};
-	},
-	fromFirestore: (snapshot: any, options: any) => {
-		const data = snapshot.data(options);
-		return new Cube(data.name, data.price, data.link);
-	},
+  toFirestore: (cube: Cube): firestore.DocumentData => {
+    return {
+      name: cube.name,
+      price: cube.price,
+      link: cube.link,
+    };
+  },
+  fromFirestore: (snapshot: firestore.QueryDocumentSnapshot): Cube => {
+    const data = snapshot.data();
+    return new Cube(data.name, data.price, data.link);
+  },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,100 +11,105 @@ const db = admin.firestore();
 const gmailEmail = functions.config().gmail.email;
 const gmailPassword = functions.config().gmail.password;
 const mailTransport = createTransport({
-	service: "gmail",
-	auth: {
-		user: gmailEmail,
-		pass: gmailPassword,
-	},
+  service: "gmail",
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
 });
 
-const APP_NAME = "New Cubes";
+const APP_NAME = "New Cubes Notifications";
 
-const sendEmail = async (emails: string[], text = "test") => {
-	try {
-		const mailOptions = {
-			from: `${APP_NAME} <${gmailEmail}>`,
-			bcc: emails,
-			subject: "New Cube",
-			text: text,
-		};
-		await mailTransport.sendMail(mailOptions);
-		console.log("New cube email sent to:", emails);
-		return null;
-	} catch (e) {
-		return e;
-	}
+const sendEmail = async (
+  emails: string[],
+  text: string,
+  subject: string = "New Cube"
+): Promise<void> => {
+  try {
+    const mailOptions = {
+      from: `${APP_NAME} <${gmailEmail}>`,
+      bcc: emails,
+      subject: subject,
+      text: text,
+    };
+    await mailTransport.sendMail(mailOptions);
+    console.log("New cube email sent to:", emails);
+  } catch (e) {}
 };
 
-const getEmailsAdresses = async () => {
-	const emails: string[] = [];
-	const emailColl = await db.collection("mails").get();
-	emailColl.forEach((doc: any) => {
-		emails.push(doc.data().mail);
-	});
-	return emails;
+const getEmailsAdresses = async (): Promise<string[]> => {
+  const emails: string[] = [];
+  const emailColl = await db.collection("mails").get();
+  emailColl.forEach((doc: any) => {
+    emails.push(doc.data().mail);
+  });
+  return emails;
 };
 
 export const sendEmailOnCreation = functions.firestore
-	.document("cubes/{cubeid}")
-	.onCreate(async (snap, _) => {
-		const emails = await getEmailsAdresses();
-		const data = snap.data();
+  .document("cubes/{cubeid}")
+  .onCreate(async (snap, _) => {
+    const emails = await getEmailsAdresses();
+    const data = snap.data();
+    if (data) {
+      return sendEmail(emails, JSON.stringify(data), data.name);
+    }
+  });
 
-		return sendEmail(emails, JSON.stringify(data));
-	});
+const getCubes = async (): Promise<Cube[]> => {
+  const html = await rp("http://www.ziicube.com/");
+  const cubesCheerioObjs = $(".txt", html);
+  const cubes: Cube[] = [];
+  cubesCheerioObjs.each((_, cube) => {
+    const price = $(".price", cube)[0].firstChild.data;
+    const namePart = cube.children[3].children[0];
+    const link = namePart.attribs.href;
+    const name = namePart.firstChild.data;
+    if (price !== undefined && name !== undefined && link !== undefined) {
+      cubes.push(new Cube(name, price, link));
+    }
+  });
 
-const getCubes = async () => {
-	const html = await rp("http://www.ziicube.com/");
-	const cubesCheerioObjs = $(".txt", html);
-	const cubes: Cube[] = [];
-	cubesCheerioObjs.each((_, cube) => {
-		const price = $(".price", cube)[0].firstChild.data;
-		const namePart = cube.children[3].children[0];
-		const link = namePart.attribs.href;
-		const name = namePart.firstChild.data;
-		if (price !== undefined && name !== undefined && link !== undefined) {
-			cubes.push(new Cube(name, price, link));
-		}
-	});
-
-	return cubes;
+  return cubes;
 };
 
-const sanitizeDocName = (name: string) => {
-	return name.replace(/\//g, "\\");
+const sanitizeDocName = (name: string): string => {
+  return name.replace(/\//g, "\\");
 };
 
-const cubesHandler = async () => {
-	try {
-		console.log("Trying to get cubes");
-		const cubes = await getCubes();
-		const cubesCollection = db.collection("cubes");
-		console.log(cubes);
-		for (const cube of cubes) {
-			const doc = cubesCollection.doc(sanitizeDocName(cube.name));
-			const getDoc = await doc.get();
-			if (getDoc.exists) {
-				// mby compare other data like link and price
-			} else {
-				//@ts-ignore
-				await doc.withConverter(cubeConverter).set(cube);
-			}
-		}
-		return [cubes];
-	} catch (e) {
-		console.error(e);
-		return [e];
-	}
+const cubesHandler = async (): Promise<Cube[]> => {
+  try {
+    console.log("Trying to get cubes");
+    const cubes = await getCubes();
+    const cubesCollection = db.collection("cubes");
+    console.log(cubes);
+    for (const cube of cubes) {
+      const doc = cubesCollection.doc(sanitizeDocName(cube.toString()));
+      const getDoc = await doc.get();
+      if (getDoc.exists) {
+        await doc.withConverter(cubeConverter).update(cube);
+      } else {
+        await doc.withConverter(cubeConverter).set(cube);
+      }
+    }
+    return cubes;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 };
 
-export const cubeTest = functions.https.onRequest(async (req, res) => {
-	const cubes = await cubesHandler();
-	res.send(cubes);
-});
+export const cubeTest = functions.https.onRequest(
+  async (req, res): Promise<void> => {
+    const cubes = await cubesHandler();
+    res.send(cubes);
+  }
+);
 
 export const scheduledFunctionPlainEnglish = functions.pubsub
-	.schedule("every 5 minutes")
-	.onRun(async context => {
-		await cubesHandler();
-	});
+  .schedule("every 30 minutes")
+  .onRun(
+    async (): Promise<void> => {
+      await cubesHandler();
+    }
+  );
